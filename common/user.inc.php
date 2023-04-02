09cc9ea32b094894c9d3112225c17b731d0495a5
malo <77111396+malo-perso@users.noreply.github.com>
2023-04-02 14:48:41 +0200

    Create user.inc.php

---
+++ common/user.inc.php
@@ -0,0 +1,34 @@
+<?php
+
+class User {
+
+    private $nom;
+    private $prenom;
+    private $mail;
+    private $mdp;  
+      
+    public function __construct($n="",$p="",$ma="",$md="") {
+        $this->nom = $n;
+        $this->prenom = $p;
+        $this->mail = $ma;
+        $this->mdp = $md;
+    }
+
+    public function getNom() { return $this->nom; }
+    public function getPrenom() { return $this->prenom;}
+    public function getMail() { return $this->mail; }
+    public function getMdp() { return $this->mdp; }
+
+    public function __toString() {
+        $res = "nom:".$this->nom."\n";
+        $res = $res ."prenom:".$this->prenom."\n";
+        $res = $res ."mail:".$this->mail."\n";
+        $res = $res ."mdp:".$this->mdp."\n";
+        return $res;
+    }
+}
+
+//test
+//$unUser = new User("nom1","prenom1","mail1","mdp1");
+//echo $unUser;
+?>
