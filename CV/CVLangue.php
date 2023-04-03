df6ec3b72f0cc24b6d41e964105c9dd8df5e61eb
Piki <alexandradesplan@gmail.com>
2023-04-03 16:28:56 +0200

    Arborescence du nav

--- CV/CVLangue.php
+++ CV/CVLangue.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>CV Langue</title>
+    <title>PageCVExpérience</title>
     <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu">
@@ -19,23 +19,13 @@
 </head>
 
 <body style="border-color: var(--color-brown);">
-    <div style="width: 155px;background: #e7e4df;border-style: solid;border-color: var(--color-brown);position: fixed;height: 900px;">
-        <ul class="nav nav-tabs flex-column" style="border-style: none;">
-            <li class="nav-item"><a class="nav-link" href="#" style="color: var(--bs-body-color);">Page accueil</a></li>
-            <li class="nav-item"><a class="nav-link active" href="#" style="color: var(--bs-body-color);background: #c8b79c;border-color: var(--bs-body-bg);border-left: px solid var(--color-blue);">CV</a>
-                <ul class="nav nav-tabs" style="border-style: none;">
-                    <li class="nav-item"><a class="nav-link" href="#" style="border-style: none;color: var(--bs-body-color);width: 150px;">Coordonnées</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#" style="color: var(--bs-body-color);width: 150px;">Formations</a></li>
-                    <li class="nav-item"><a class="nav-link" style="color: var(--bs-body-color);background: #e7e4df;width: 150px;">Expériences</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#" style="color: var(--bs-body-color);width: 150px;background: #e7e4df;">Compétences</a></li>
-                    <li class="nav-item"><a class="nav-link active" href="#" style="color: var(--bs-body-color);width: 150px;background: #c8b79c;">Langues</a></li>
-                </ul>
-            </li>
-            <li class="nav-item"><a class="nav-link" href="#" style="border-style: none;color: var(--bs-body-color);">Compétences</a></li>
-            <li class="nav-item"><a class="nav-link" href="#" style="color: var(--bs-body-color);">Projets</a></li>
-            <li class="nav-item"><a class="nav-link" href="#" style="border-style: solid;color: var(--bs-body-color);">Contacts</a></li>
-        </ul>
-    </div>
+    <?php  
+
+        include '../common/nav.inc.php';
+
+        getNav();
+
+    ?>
     <section class="text-start" style="text-align: left;padding-top: 60px;padding-left: 186px;padding-right: 30px;">
         <h2 class="text-start d-xxl-flex align-items-center" style="color: var(--bs-body-color);margin-top: 0px;padding-top: 0px;padding-left: 0px;width: 253px;margin-left: 160px;">Expériences</h2>
         <div class="container-fluid text-start d-xl-flex align-items-center justify-content-xl-center profile profile-view" id="profile" style="width: 850px;height: 190.25px;border-style: solid;padding-right: 0px;margin-right: 26px;margin-top: 0px;padding-top: 0px;padding-left: 0px;margin-left: 159px;">
@@ -70,7 +60,7 @@
             </form>
         </div>
     </section>
-    <button class="btn btn-primary" data-bss-hover-animate="pulse" type="button" style="margin-left: 189px;border-style: solid;border-color: var(--color-brown);background: rgba(255,255,255,0.5);color: var(--color-brown);margin-top: 45px;"><a href="CVCompetence.html" style="color: var(--color-brown)">Précédent (compétences)</a></button>
+    <button class="btn btn-primary" data-bss-hover-animate="pulse" type="button" style="margin-left: 189px;border-style: solid;border-color: var(--color-brown);background: rgba(255,255,255,0.5);color: var(--color-brown);margin-top: 45px;"><a href="competenceCV.html" style="color: var(--color-brown)">Précédent (compétences)</a></button>
     <button class="btn btn-primary float-end" data-bss-hover-animate="pulse" type="button" style="border-style: solid;border-color: var(--color-brown);background: var(--color-brown);color: var(--bs-body-bg);margin-top: 45px;margin-left: 0px;margin-right: 21px;">Enregistrer</button>
     <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="../assets/js/bs-init.js"></script>
--- common/nav.inc.php
+++ common/nav.inc.php
@@ -4,14 +4,14 @@ function getNav()
 {
     echo "<div style=\"width: 155px;background: #e7e4df;border-style: solid;border-color: var(--color-brown);position: fixed;height: 900px;padding-top: 0px;margin-top: 0px;\">\n";
     echo "<ul class=\"nav nav-tabs flex-column\" style=\"border-style: none;\">\n";
-    echo "<li class=\"nav-item\"><a class=\"nav-link\" href=\"#\" style=\"color: var(--bs-body-color);\">Page accueil</a></li>\n";
-    echo "<li class=\"nav-item\"><a class=\"nav-link active\" href=\"#\" style=\"color: var(--bs-body-color);background: #c8b79c;border-color: var(--bs-body-bg);border-left: px solid var(--color-blue);\">CV</a>\n";
+    echo "<li class=\"nav-item\"><a class=\"nav-link\" href=\"../accueil.php\" style=\"color: var(--bs-body-color);\">Page accueil</a></li>\n";
+    echo "<li class=\"nav-item\"><a class=\"nav-link active\" href=\"../CV/CVResume.php\" style=\"color: var(--bs-body-color);background: #c8b79c;border-color: var(--bs-body-bg);border-left: px solid var(--color-blue);\">CV</a>\n";
     echo "<ul class=\"nav nav-tabs\" style=\"border-style: none;\">\n";
-    echo "<li class=\"nav-item\"><a class=\"nav-link active\" href=\"#\" style=\"border-style: none;color: var(--bs-body-color);width: 150px;background: #e7e4df;\">Coordonées</a></li>\n";
-    echo "<li class=\"nav-item\"><a class=\"nav-link\" href=\"#\" style=\"color: var(--bs-body-color);width: 150px;background: #c8b79c;\">Formations</a></li>\n";
-    echo "<li class=\"nav-item\"><a class=\"nav-link\" href=\"#\" style=\"color: var(--bs-body-color);background: #e7e4df;width: 150px;\">Expériences</a></li>\n";
-    echo "<li class=\"nav-item\"><a class=\"nav-link\" href=\"#\" style=\"color: var(--bs-body-color);width: 150px;\">Compétences</a></li>\n";
-    echo "<li class=\"nav-item\"><a class=\"nav-link\" href=\"#\" style=\"color: var(--bs-body-color);width: 150px;\">Langues</a></li>\n";
+    echo "<li class=\"nav-item\"><a class=\"nav-link active\" href=\"../CV/CVCoordonnees.php\" style=\"border-style: none;color: var(--bs-body-color);width: 150px;background: #e7e4df;\">Coordonnées</a></li>\n";
+    echo "<li class=\"nav-item\"><a class=\"nav-link\" href=\"../CV/CVFormation.php\" style=\"color: var(--bs-body-color);width: 150px;background: #c8b79c;\">Formations</a></li>\n";
+    echo "<li class=\"nav-item\"><a class=\"nav-link\" href=\"../CV/CVExperience.php\" style=\"color: var(--bs-body-color);background: #e7e4df;width: 150px;\">Expériences</a></li>\n";
+    echo "<li class=\"nav-item\"><a class=\"nav-link\" href=\"../CV/CVCompetence.php\" style=\"color: var(--bs-body-color);width: 150px;\">Compétences</a></li>\n";
+    echo "<li class=\"nav-item\"><a class=\"nav-link\" href=\"../CV/CVLangue.php\" style=\"color: var(--bs-body-color);width: 150px;\">Langues</a></li>\n";
     echo "</ul>\n";
     echo "</li>\n";
     echo "<li class=\"nav-item\"><a class=\"nav-link\" href=\"#\" style=\"border-style: none;color: var(--bs-body-color);\">Compétences</a></li>\n";
