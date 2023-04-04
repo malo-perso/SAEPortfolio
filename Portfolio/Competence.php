2e4292055571fc65a0ca94a5b0199ec4ab14411b
Aanaelle <103115892+Aanaelle@users.noreply.github.com>
2023-04-04 16:34:04 +0200

    update editor.js

--- Portfolio/Competence.php
+++ Portfolio/Competence.php
@@ -28,8 +28,7 @@
     ?>
     <h1 class="text-center" style="margin-left: 152px;margin-top: 0px;padding-top: 30px;">Page de Compétences</h1>
     
-    <div id="editorjs" style="margin-left: 160px; margin-right: 5px; border: 1px black solid;">
-    </div>
+    <div id=editorjs style="margin-top: 80px; margin-right:30px;margin-left: 185px;padding:0px;border:solid"></div>
     
     <script src="ckeditor/ckeditor.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/@editorjs/header@latest"></script>
@@ -38,6 +37,8 @@
         <script src="https://cdn.jsdelivr.net/npm/@editorjs/checklist@latest"></script>
         <script src="https://cdn.jsdelivr.net/npm/@editorjs/quote@latest"></script>
         <script src="https://cdn.jsdelivr.net/npm/@editorjs/embed@latest"></script>
+        <script src="https://cdn.jsdelivr.net/npm/editorjs-text-color-plugin@2.0.2/dist/bundle.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/editorjs-paragraph-with-alignment@3.0.0"></script>
 
 
         <!-- Load Editor.js's Core -->
@@ -57,6 +58,27 @@
             list: {
                 class: List,
                 inlineToolbar: true
+            },
+            Color: {
+            class: ColorPlugin, // if load from CDN, please try: window.ColorPlugin
+            config: {
+                colorCollections: ['#EC7878','#9C27B0','#673AB7','#3F51B5','#0070FF','#03A9F4','#00BCD4','#4CAF50','#8BC34A','#CDDC39', '#FFF'],
+                defaultColor: '#FF1300',
+                type: 'text', 
+                customPicker: true // add a button to allow selecting any colour  
+            }     
+            },
+            Marker: {
+            class: ColorPlugin, // if load from CDN, please try: window.ColorPlugin
+            config: {
+                defaultColor: '#FFBF00',
+                type: 'marker',
+                icon: `<svg fill="#000000" height="200px" width="200px" version="1.1" id="Icons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M17.6,6L6.9,16.7c-0.2,0.2-0.3,0.4-0.3,0.6L6,23.9c0,0.3,0.1,0.6,0.3,0.8C6.5,24.9,6.7,25,7,25c0,0,0.1,0,0.1,0l6.6-0.6 c0.2,0,0.5-0.1,0.6-0.3L25,13.4L17.6,6z"></path> <path d="M26.4,12l1.4-1.4c1.2-1.2,1.1-3.1-0.1-4.3l-3-3c-0.6-0.6-1.3-0.9-2.2-0.9c-0.8,0-1.6,0.3-2.2,0.9L19,4.6L26.4,12z"></path> </g> <g> <path d="M28,29H4c-0.6,0-1-0.4-1-1s0.4-1,1-1h24c0.6,0,1,0.4,1,1S28.6,29,28,29z"></path> </g> </g></svg>`
+                }       
+            },
+            paragraph: {
+                class: Paragraph,
+                inlineToolbar: true,
             },
             checklist: {
             class: Checklist,
