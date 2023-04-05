708e53db4591a8ff3e77d630640e2acdfa00e445
malo <77111396+malo-perso@users.noreply.github.com>
2023-04-05 15:56:49 +0200

    Update competence.inc.php

--- common/competence.inc.php
+++ common/competence.inc.php
@@ -5,14 +5,8 @@ class Competence implements \JsonSerializable {
 
     private $nomComp;
 
-<<<<<<< Updated upstream
     public function __construct($n="") {
 	    $this->nomComp = $n;
-=======
-    public function __construct($i=-1,$d="") {
-      	$this->idComp = $i;
-	    $this->descComp = $d;
->>>>>>> Stashed changes
     }
 
     public function getNomComp() { return $this->nomComp; }
