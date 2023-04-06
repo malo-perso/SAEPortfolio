4fcebfd0da2146d22102b9b9e79e7e78b6a99dbd
Piki <alexandradesplan@gmail.com>
2023-04-06 15:29:38 +0200

    nav en js

--- assets/js/getNav.js
+++ assets/js/getNav.js
@@ -4,33 +4,35 @@ function getNav(pageEnCours)
     const arrayNom = ["", "Accueil", "CV", "Coordonnées", "Formation", "Expérience", "Compétences", "Langues", "Compétences", "Projets", "Contact"];
 
     let navHtml = "<div id=\"nav\" style=\"width: 15%;background: #e7e4df;border-style: solid;border-color: var(--color-brown);position: fixed;height: 100%>\n" +
-                    "<ul class=\"nav nav-tabs flex-column\" style=\"border-style: none;">";
+                    "<ul class=\"nav nav-tabs flex-column\" style=\"border-style: none;\">";
 
     if (array[0] === pageEnCours) 
     {
         navHtml += "<li class=\"nav-item\"><a class=\"nav-link active\" href=\"../${array[0]}\" style=\"color: var(--bs-body-color);background: #c8b79c;border-color: var(--bs-body-bg);margin-bottom:5%;\"><img src=\"../images/logoAcc.png\" alt=\"Logo\" style=\"width: 20%;\"></a></li>";
     } else {
-        navHtml += "<li class="nav-item"><a class="nav-link" href="../${array[0]}" style="color: var(--bs-body-color);margin-bottom:5%;"><img src="../images/logoAcc.png" alt="Logo" style="width: 20%;"></a></li>";
+        navHtml += "<li class=\"nav-item\"><a class=\"nav-link\" href=\"../${array[0]}\" style=\"color: var(--bs-body-color);margin-bottom:5%;\"><img src=\"../images/logoAcc.png\" alt=\"Logo\" style=\"width: 20%;\"></a></li>";
     }
 
-    for (let i = 1; i < array.length; i++) {
-        if (i > 2 && i < 8) {
-            if (array[i] === pageEnCours) {
-                navHtml += "<li class="nav-item"><a class="nav-link active" href="./${array[i]}" style="color: var(--bs-body-color);background: #c8b79c;border-color: var(--bs-body-bg); padding-left:15%;">${arrayNom[i]}</a></li>";
+    for (let i = 1; i < array.length; i++) 
+    {
+        if (i > 2 && i < 8) 
+        {
+            if (array[i] === pageEnCours) 
+            {
+                navHtml += "<li class=\"nav-item\"><a class=\"nav-link active\" href=\"./${array[i]}\" style=\"color: var(--bs-body-color);background: #c8b79c;border-color: var(--bs-body-bg); padding-left:15%;\">${arrayNom[i]}</a></li>";
             } else {
-                navHtml += "<li class="nav-item"><a class="nav-link" href="./${array[i]}" style="color: var(--bs-body-color);padding-left:15%;">${arrayNom[i]}</a></li>";
+                navHtml += "<li class=\"nav-item\"><a class=\"nav-link\" href=\"./${array[i]}\" style=\"color: var(--bs-body-color);padding-left:15%;\">${arrayNom[i]}</a></li>";
             }
         } else {
             if (array[i] === pageEnCours) {
-                navHtml += "<li class="nav-item"><a class="nav-link active" href="./${array[i]}" style="color: var(--bs-body-color);background: #c8b79c;border-color: var(--bs-body-bg);">${arrayNom[i]}</a></li>";
+                navHtml += "<li class=\"nav-item\"><a class=\"nav-link active\" href=\"./${array[i]}\" style=\"color: var(--bs-body-color);background: #c8b79c;border-color: var(--bs-body-bg);\">${arrayNom[i]}</a></li>";
             } else {
-                navHtml += "<li class="nav-item"><a class="nav-link" href="./${array[i]}" style="color: var(--bs-body-color);">${arrayNom[i]}</a></li>";
+                navHtml += "<li class=\"nav-item\"><a class=\"nav-link\" href=\"./${array[i]}\" style=\"color: var(--bs-body-color);\">${arrayNom[i]}</a></li>";
             }
         }
     }
 
-    navHtml += "</ul>
-    </div>`;
+    navHtml += "</ul></div>";
 
     return navHtml;
 }
