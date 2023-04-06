63680255edca2ae3fa7bcc514357427627613b7e
Piki <alexandradesplan@gmail.com>
2023-04-06 23:01:46 +0200

    style de la page Competence reglé

--- Portfolio/CVFormation.php
+++ Portfolio/CVFormation.php
@@ -31,14 +31,24 @@
 
         </script>
         <section class="text-start" style="margin-left:15%; width:85%">
-            <h2 class="text-start d-xxl-flex align-items-center" style="color: var(--bs-body-color);margin-top: 0px;padding-top: 0px;padding-left: 0px;width: 253px;margin-left: 160px;">Formations</h2>
-            <section style="margin: 0px 10px 10px 10px;margin-top: 34px;border-style: solid;border-color: var(--color-brown);width: 500px;padding: 22px 10px 10px 10px;margin-bottom: 14px;margin-right: 23px;padding-left: 51px;margin-left: 164px;">
-                <p class="index_nom_ville">1 : Nom de l&#39;établissement - Ville</p>
-                <p class="diplome_domaine_dates">Diplôme - domaine détude - date début | date fin</p>
-            </section>
-            <div class="container-fluid text-start d-xl-flex align-items-center justify-content-xl-center profile profile-view" id="profile" style="width: 788px;height: 504.25px;border-style: solid;padding-left: 11px;padding-right: 0px;margin-right: 26px;margin-left: 156px;">
-                <form style="height: 405px;width: 817.6px;">
-                    <div class="row profile-row" style="height: 327px;width: 880.6px;margin-right: -116px;margin-top: 6px;margin-left: 103px;">
+
+            <h2 class="text-center" style="color: var(--bs-body-color);padding-top: 5%;">Formations</h2>
+
+            <div class="container-fluid text-start d-xl-flex align-items-center justify-content-xl-center profile profile-view" style="margin-top:5%;width:40%; border-style: solid;border-color: var(--color-brown);">
+                <div class="col" style=padding:2%;>
+                        <div class="row">
+                            <p class="index_nom_ville">1 : Nom de l&#39;établissement - Ville</p>
+                        </div>
+                    <div class="row">
+                        <p class="diplome_domaine_dates">Diplôme - domaine détude - date début | date fin</p>
+                    </div>
+                </div>
+            </div>
+
+            
+            <div class="container-fluid text-start d-xl-flex align-items-center justify-content-xl-center profile profile-view" id="profile" style="width: 60%;height: 40%;border: 1px solid; margin-top:5%;">
+                <form style="width:70%;">
+                    <div class="row profile-row" style="margin-right: -60%;padding-right: 48px;">
                         <div class="col-md-8">
                             <div class="row">
                                 <div class="col-sm-12 col-md-6">
@@ -89,7 +99,7 @@
                             </div>
                             <div class="row">
                                 <div class="col-sm-12 col-md-6"><label class="form-label form-label form-label form-label">Date début</label>
-                                    <div class="d-lg-flex align-items-lg-center form-group mb-3"><select class="form-select form-select form-select" style="width: 123.4px;margin-right: 10px;">
+                                    <div class="d-lg-flex align-items-lg-center form-group mb-3"><select class="form-select form-select form-select">
                                             <optgroup label="Mois">
                                                 <option value="" selected="">Mois</option>
                                                 <option value="1">Janvier</option>
@@ -105,10 +115,10 @@
                                                 <option value="11">Novembre</option>
                                                 <option value="12">Décembre</option>
                                             </optgroup>
-                                        </select><input class="form-control" type="number" min="1958" max="2033" name="anneeDeb" style="width: 123px;"></div>
+                                        </select><input class="form-control" type="number" min="1958" max="2033" name="anneeDeb"></div>
                                 </div>
                                 <div class="col-sm-12 col-md-6"><label class="form-label form-label form-label form-label">Date fin</label>
-                                    <div class="d-lg-flex align-items-lg-center form-group mb-3"><select class="form-select form-select form-select" style="width: 123.4px;margin-right: 10px;">
+                                    <div class="d-lg-flex align-items-lg-center form-group mb-3"><select class="form-select form-select form-select">
                                             <optgroup label="Mois">
                                                 <option value="" selected="">Mois</option>
                                                 <option value="1">Janvier</option>
@@ -124,7 +134,7 @@
                                                 <option value="11">Novembre</option>
                                                 <option value="12">Décembre</option>
                                             </optgroup>
-                                        </select><input class="form-control" type="number" min="1958" max="2033" name="anneeFin" style="width: 123px;"></div>
+                                        </select><input class="form-control" type="number" min="1958" max="2033" name="anneeFin"></div>
                                 </div>
                             </div>
                             <div class="row">
@@ -135,9 +145,10 @@
                 </form>
             </div>
         
-        <button class="btn btn-primary" data-bss-hover-animate="pulse" type="button" style="margin-left: 189px;border-style: solid;border-color: var(--color-brown);background: rgba(255,255,255,0.5);color: var(--color-brown);margin-top: 45px;"><a href="CVCoordonnees.php" style="color: var(--color-brown)">Précédent (coordonnées)</a></button>
-        <button class="btn btn-primary float-end" data-bss-hover-animate="pulse" type="button" style="margin-top: 40px;margin-right: 27px;background: rgba(255,255,255,0.5);color: var(--color-brown);border-style: solid;border-color: var(--color-brown);"><a href="CVExperience.php" style="color: var(--color-brown)">Suivant (expérience)</a></button>
+            <button class="btn btn-primary" data-bss-hover-animate="pulse" type="button" style="margin-top: 2%;margin-left: 189px;border-style: solid;border-color: var(--color-brown);background: rgba(255,255,255,0.5);color: var(--color-brown);margin-bottom: 45px;"><a href="CVCoordonnees.php" style="color: var(--color-brown)">Précédent (coordonnées)</a></button>
+            <button class="btn btn-primary float-end" data-bss-hover-animate="pulse" type="button" style="margin-top: 2%;margin-left: 14px;margin-bottom: 44px;border-color: var(--color-brown);background: rgb(255,255,255);color: var(--color-brown);margin-right: 201px;"><a href="CVExperience.php" style="color: var(--color-brown)">Suivant (expérience)</a></button>
 
+        
     </section>
     </div>
     <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
