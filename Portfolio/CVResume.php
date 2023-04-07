d57e77502bf4783e31bbb7504559ae5f97cc1d8a
Aanaelle <103115892+Aanaelle@users.noreply.github.com>
2023-04-07 09:27:34 +0200

    update

--- Portfolio/CVResume.php
+++ Portfolio/CVResume.php
@@ -46,12 +46,49 @@
                 </div>
             </div>
     </div>
+    <div id="templates" style="border:1px var(--color-brown) solid; display:none;">
+
+        <div
+            style="background-color: var(--color-brown); color: var(--color-white); padding: 10px; margin-bottom: 10px; text-align: center;">
+            <h3>Choisissez une template</h3>
+        </div>
+
+        <div  class="row" style="margin-left:20%; margin-bottom:2%">
+            <div id="tmpl1" class="col-8"  style="width:auto; ">
+                <h3>Template 1</h3>
+                <img src="../images/template1.png" id="img1" alt="Template 1" style="width:40%; ">
+            </div>
+            <div id="tmpl2" class="col-8" style="width:auto; ">
+                <h3>Template 2</h3>
+                <img src="../images/template2.png" id="img2" alt="Template 2" style="width:40%; ">
+            </div>
+        </div>
+    </div>
    
     <script>
+
+        tmpl1 = document.getElementById("img1");
+        tmpl2 = document.getElementById("img2");
+        resume = document.getElementById("resume");
         //event listener for the button
         document.getElementById("choixTemplate").addEventListener("click", function() {
 
+            //get the div
+            var div = document.getElementById("templates");
+            div.style.display = "block";
 
+            tmpl1.addEventListener("click", function() {
+                console.log("Template 1");
+                tmpl1.style.border = "2px solid var(--color-brown)";
+                tmpl2.style.border = "none";
+            });
+
+            tmpl2.addEventListener("click", function() {
+                console.log("Template 2");
+                tmpl2.style.border = "2px solid var(--color-brown)";
+                tmpl1.style.border = "none";
+            });
+                     
             console.log("Choisissez une template");
         });
     </script>
