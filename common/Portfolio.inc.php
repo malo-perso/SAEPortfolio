d58634e3d9fbbfbf9687ca51e26293f0f186a4dc
malo <77111396+malo-perso@users.noreply.github.com>
2023-04-07 22:38:36 +0200

    update Portfolio.inc.php

--- common/Portfolio.inc.php
+++ common/Portfolio.inc.php
@@ -4,21 +4,25 @@ class portfolio {
     private $idPortfolio;
     private $nomPortfolio;
     private $estPublic;
+    private $idUser;
       
-    public function __construct($i="",$n="",$e="") {
+    public function __construct($i="",$n="",$e="", $u="") {
         $this->idPortfolio = $i;
         $this->nomPortfolio = $n;
         $this->estPublic = $e;
+        $this->idUser = $u;
     }
 
     public function getIdPortfolio() { return $this->idPortfolio; }
     public function getNomPortfolio() { return $this->nomPortfolio;}
     public function getEstPublic() { return $this->estPublic; }
+    public function getIdUser() { return $this->idUser; }
 
     public function __toString() {
         $res = "idPortfolio:".$this->idPortfolio."\n";
         $res = $res ."nomPortfolio:".$this->nomPortfolio."\n";
         $res = $res ."estPublic:".$this->estPublic."\n";
+        $res = $res ."idUser:".$this->idUser."\n";
         return $res;
     }
 }
