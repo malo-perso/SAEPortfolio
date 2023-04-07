3283165f4001fc6f83c84c96b8627f623aade5b0
Aanaelle <103115892+Aanaelle@users.noreply.github.com>
2023-04-07 11:06:20 +0200

    il manquait un ;

--- connexion.php
+++ connexion.php
@@ -110,7 +110,7 @@ else {
                if(emailValide($login) && motDePasseValide($mdp)){
                     if($db->isemailOK($login) && $db->isMotDePasseOK($login,$mdp)){
                          //remplir les informations de l'utilisateur dans la session
-                         $IdUser = $db->getUserID($login)
+                         $IdUser = $db->getUserID($login);
                          echo "Login et mot de passe correct";
                          $nom = $db->getNom($login);
                          $prenom = $db->getPrenom($login);
